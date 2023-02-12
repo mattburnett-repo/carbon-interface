58aeb91c1d8863cb4dc51abf4f733a53f5be89e8
Matt Burnett <mburnett@tutanota.de>
2023-02-12 19:14:40 +0100

    chore: added analytics for react

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import { inject } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 
 import App from './App'
 import './styles/index.css'
-
-inject()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 )
